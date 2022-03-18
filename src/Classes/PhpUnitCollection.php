99d8b6fe6a4204d78f7c1ad64fac7c809a845a74
andreas.wunderwald <wundii@gmail.com>
2022-03-18 08:49:58 +0100

    release Version 0.1.24

--- src/Classes/PhpUnitCollection.php
+++ src/Classes/PhpUnitCollection.php
@@ -12,13 +12,28 @@ final class PhpUnitCollection implements Iterator
     use TraitIterator;
 
     /**
+     * @param string $status
      * @param string $description
      * @return void
      */
-    public static function addEntity(string $description)
+    public static function addEntity(string $status, string $description)
     {
+        $backtrace = debug_backtrace();
+        $filePath  = $backtrace[0]['file'] ?? '';
+        $fileLine  = $backtrace[0]['line'] ?? null;
+        $fileClass = $backtrace[1]['function'] ?? '';
+        $fileFunc  = $backtrace[1]['class'] ?? '';
+
+        $entity = new PhpUnitEntity();
+        $entity->setStatus($status);
+        $entity->setDescription($description);
+        $entity->setFilePath($filePath);
+        $entity->setFileLine($fileLine);
+        $entity->setFileClass($fileClass);
+        $entity->setFileFunc($fileFunc);
+
         $instance = self::create();
-        $instance->collection[] = $description;
+        $instance->collection[] = $entity;
     }
 
     /**
@@ -30,10 +45,11 @@ final class PhpUnitCollection implements Iterator
     }
 
     /**
+     * @param string $status
      * @param string $description // str_contains
      * @return bool
      */
-    public static function isEntityExists(string $description): bool
+    public static function isEntityExists(string $status, string $description): bool
     {
         $instance = self::create();
         foreach($instance AS $entity)
@@ -45,5 +61,111 @@ final class PhpUnitCollection implements Iterator
         }
 
         return false;
+    }
+}
+
+final class PhpUnitEntity
+{
+    private string $status      = '';
+    private string $description = '';
+    private string $filePath    = '';
+    private ?int   $fileLine    = null;
+    private string $fileClass   = '';
+    private string $fileFunc    = '';
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFilePath(): string
+    {
+        return $this->filePath;
+    }
+
+    /**
+     * @param string $filePath
+     */
+    public function setFilePath(string $filePath): void
+    {
+        $this->filePath = $filePath;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getFileLine(): ?int
+    {
+        return $this->fileLine;
+    }
+
+    /**
+     * @param int|null $fileLine
+     */
+    public function setFileLine(?int $fileLine): void
+    {
+        $this->fileLine = $fileLine;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileClass(): string
+    {
+        return $this->fileClass;
+    }
+
+    /**
+     * @param string $fileClass
+     */
+    public function setFileClass(string $fileClass): void
+    {
+        $this->fileClass = $fileClass;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileFunc(): string
+    {
+        return $this->fileFunc;
+    }
+
+    /**
+     * @param string $fileFunc
+     */
+    public function setFileFunc(string $fileFunc): void
+    {
+        $this->fileFunc = $fileFunc;
     }
 }
